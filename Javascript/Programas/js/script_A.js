991bd443a786170730515c08afc904c830b902fb
LucasLima021 <lucaslimabjflu@gmail.com>
2021-05-11 12:21:45 -0300

    update

--- Javascript/Programas/js/script_A.js
+++ Javascript/Programas/js/script_A.js
@@ -9,11 +9,14 @@ function horas() {
 
     if (hora > 0 && hora < 12) {
         imagem.src = "img/manha.jpg"
+        document.body.style.background = "#e2cd9f"
     }
     else if (hora < 18) {
         imagem.src = "img/tarde.jpg"
+        document.body.style.background = "#b9846f"
     }
     else {
         imagem.src = "img/noite.jpg"
+        document.body.style.background = "#515154"
     }
 }
